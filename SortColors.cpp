5a0bbd7e83632e1d5f609ece15f5403a7cfcd096
haweteng <115398931+haweteng@users.noreply.github.com>
2023-01-22 14:40:19 +0300

    Create SortColors.cpp

---
+++ SortColors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0; i<nums.size();i++){
+            for(int j=0; j<nums.size();j++){
+                if(nums[j]>nums[i]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+    }
+};
